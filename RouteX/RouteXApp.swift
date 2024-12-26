d0c93c6dc87a3f680d26324e0e62cb4b586c40e4
BINAYA THAPA MAGAR <binayathapamagar@BINAYAs-MacBook-Pro.local>
2024-12-26 18:12:22 -0500

    First push

---
+++ RouteX/RouteXApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteXApp.swift
+//  RouteX
+//
+//  Created by BINAYA THAPA MAGAR on 2024-12-26.
+//
+
+import SwiftUI
+
+@main
+struct RouteXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
